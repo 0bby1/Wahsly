b872c9bc15b0cf0fa4094307461b8b5df18eab8c
0bby1 <cesarava06@gmail.com>
2026-08-20 12:49:16 -0600

    Prueba de mensaje

--- app/src/main/java/com/example/wahsly/MainActivity.kt
+++ app/src/main/java/com/example/wahsly/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.wahsly.ui.theme.WahslyTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,5 +30,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun App(){
-
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFFFFCE8)),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Prueba de Github",
+            fontSize = 32.sp
+        )
+    }
 }

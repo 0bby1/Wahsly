715e87b5e4583c8c7bff81be621940a1af489469
Raf1ta <AL05077696@tecmilenio.mx>
2026-09-04 21:29:50 -0600

    Agreguelo de la base de datos 🦀:3

--- app/src/main/java/com/example/wahsly/MainActivity.kt
+++ app/src/main/java/com/example/wahsly/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -14,6 +15,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
+        base_de_datos_Usuarios.inicializar(this)
+        BaseDatosHistorial.inicializar(this)
         setContent {
             MaterialTheme {
                 // Primera pantalla que se mostrará

2449b9a3831bdcdac11ecaeca6c5596c17a43d01
0bby1 <cesarava06@gmail.com>
2026-09-07 13:18:14 -0600

    Navegacion entre pantallas con NavController Implementado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,5 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("androidx.navigation:navigation-compose:2.9.8")
 }
